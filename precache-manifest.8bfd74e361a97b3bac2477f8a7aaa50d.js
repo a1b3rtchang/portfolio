1d42cf5850ca7b78fa567105bc29402341127260
a1b3rtchang <60363701+a1b3rtchang@users.noreply.github.com>
2021-05-12 09:41:45 +0000

    Deploying to gh-pages from  @ ef8b1eec6550ddee61acb3592bf173c6bcc534d1 🚀

---
+++ precache-manifest.8bfd74e361a97b3bac2477f8a7aaa50d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143e961cfa7e66ca73a38c49f3a4b926",
+    "url": "/albert-chang.me/index.html"
+  },
+  {
+    "revision": "2bf5c8b2cbaf2dfa8d7b",
+    "url": "/albert-chang.me/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "665253152a536755e894",
+    "url": "/albert-chang.me/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "0609bd456390e757366b",
+    "url": "/albert-chang.me/static/css/main.02f01f08.chunk.css"
+  },
+  {
+    "revision": "d66601c3bfd1337b56cd",
+    "url": "/albert-chang.me/static/js/2.8e9418d3.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/albert-chang.me/static/js/2.8e9418d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf5c8b2cbaf2dfa8d7b",
+    "url": "/albert-chang.me/static/js/3.b9d37ae7.chunk.js"
+  },
+  {
+    "revision": "665253152a536755e894",
+    "url": "/albert-chang.me/static/js/4.77600a36.chunk.js"
+  },
+  {
+    "revision": "0609bd456390e757366b",
+    "url": "/albert-chang.me/static/js/main.9496077c.chunk.js"
+  },
+  {
+    "revision": "70eb934ff33ca1523de0",
+    "url": "/albert-chang.me/static/js/runtime-main.f5d0cf83.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/albert-chang.me/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/albert-chang.me/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/albert-chang.me/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/albert-chang.me/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/albert-chang.me/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/albert-chang.me/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/albert-chang.me/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/albert-chang.me/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/albert-chang.me/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/albert-chang.me/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/albert-chang.me/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/albert-chang.me/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/albert-chang.me/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/albert-chang.me/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/albert-chang.me/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/albert-chang.me/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/albert-chang.me/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
