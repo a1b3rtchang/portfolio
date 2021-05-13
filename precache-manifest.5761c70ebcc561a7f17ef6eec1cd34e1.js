36bedabfc8d663fac1e7a93c3f308b3e841c7ec4
albert chang <alberthchang@berkeley.edu>
2021-05-12 17:30:59 -0700

    Updates

---
+++ precache-manifest.5761c70ebcc561a7f17ef6eec1cd34e1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c332b24dcead9d911d7146b9d3db5b4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b53ada224dee3bec4ec0",
+    "url": "/portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "cd0587d5094e46e81707",
+    "url": "/portfolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "d5fdd13301d992391e26",
+    "url": "/portfolio/static/css/main.b4d2569a.chunk.css"
+  },
+  {
+    "revision": "ee7c3343b21fc0cd467f",
+    "url": "/portfolio/static/js/2.6d293ceb.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.6d293ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53ada224dee3bec4ec0",
+    "url": "/portfolio/static/js/3.cc907b20.chunk.js"
+  },
+  {
+    "revision": "cd0587d5094e46e81707",
+    "url": "/portfolio/static/js/4.58d6e2ae.chunk.js"
+  },
+  {
+    "revision": "d5fdd13301d992391e26",
+    "url": "/portfolio/static/js/main.c92462dc.chunk.js"
+  },
+  {
+    "revision": "80dae4de29b292453f77",
+    "url": "/portfolio/static/js/runtime-main.76405be8.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "66198a8ca4fe92752b88c38e7b433e72",
+    "url": "/portfolio/static/media/MirrorFit.66198a8c.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/portfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "81178b3da2256cc1002b79ec42eae0ea",
+    "url": "/portfolio/static/media/albert_laptop.81178b3d.png"
+  },
+  {
+    "revision": "3d77f4b2f55f57dcad89ea383033ea6c",
+    "url": "/portfolio/static/media/albert_peace.3d77f4b2.png"
+  },
+  {
+    "revision": "a8552979bb08d3d33b0f9eb012ba916f",
+    "url": "/portfolio/static/media/berkeleyLogo.a8552979.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/portfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/portfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
